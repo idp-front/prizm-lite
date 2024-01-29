ee1c60304c88ab577617b9916d34316c006c10b4
bumalai <131881448+bumalai@users.noreply.github.com>
2024-01-29 17:04:15 +0300

    feat: add card

---
+++ prizm-lite/src/lib/card/card.stories.ts
@@ -0,0 +1,37 @@
+import type { Meta, StoryObj } from '@storybook/html';
+
+type Args = {
+  label: string;
+};
+
+const meta: Meta<Args> = {
+  /* 👇 The title prop is optional.
+   * See https://storybook.js.org/docs/configure/#configure-story-loading
+   * to learn how to generate automatic titles
+   */
+  title: 'Card',
+};
+
+export default meta;
+type Story = StoryObj<Args>;
+
+/*
+ *👇 Render functions are a framework specific feature to allow you control on how the component renders.
+ * See https://storybook.js.org/docs/api/csf
+ * to learn how to use render functions.
+ */
+export const Default: Story = {
+  render: (args) => {
+    let area = document.createElement('div');
+
+    area.innerHTML = `
+<div class="prizm-card" style="max-width: 400px">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+</div>
+    `
+    return area;
+  },
+  args: {
+    label: 'Label',
+  },
+};
