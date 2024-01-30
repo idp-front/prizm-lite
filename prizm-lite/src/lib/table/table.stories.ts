d768a6795a8389b5a650a0e99a3328af059cb005
bumalai <131881448+bumalai@users.noreply.github.com>
2024-01-30 14:50:43 +0300

    table

---
+++ prizm-lite/src/lib/table/table.stories.ts
@@ -0,0 +1,105 @@
+import type { Meta, StoryObj } from '@storybook/html';
+
+type Args = {
+  label: string;
+};
+
+const meta: Meta<Args> = {
+  /* 👇 The title prop is optional.
+   * See https://storybook.js.org/docs/configure/#configure-story-loading
+   * to learn how to generate automatic titles
+   */
+  title: 'Table',
+};
+
+export default meta;
+type Story = StoryObj<Args>;
+
+/*
+ *👇 Render functions are a framework specific feature to allow you control on how the component renders.
+ * See https://storybook.js.org/docs/api/csf
+ * to learn how to use render functions.
+ */
+export const Default: Story = {
+  render: (args) => {
+    let area = document.createElement('div');
+
+    area.innerHTML = `
+<table class="prizm-table">
+  <tr>
+    <th>Company</th>
+    <th>Contact</th>
+    <th>Country</th>
+  </tr>
+  <tr>
+    <td>Alfreds Futterkiste</td>
+    <td>Maria Anders</td>
+    <td>Germany</td>
+  </tr>
+  <tr>
+    <td>Berglunds snabbköp</td>
+    <td>Christina Berglund</td>
+    <td>Sweden</td>
+  </tr>
+  <tr>
+    <td>Centro comercial Moctezuma</td>
+    <td>Francisco Chang</td>
+    <td>Mexico</td>
+  </tr>
+  <tr>
+    <td>Ernst Handel</td>
+    <td>Roland Mendel</td>
+    <td>Austria</td>
+  </tr>
+  <tr>
+    <td>Island Trading</td>
+    <td>Helen Bennett</td>
+    <td>UK</td>
+  </tr>
+  <tr>
+    <td>Königlich Essen</td>
+    <td>Philip Cramer</td>
+    <td>Germany</td>
+  </tr>
+  <tr>
+    <td>Laughing Bacchus Winecellars</td>
+    <td>Yoshi Tannamuri</td>
+    <td>Canada</td>
+  </tr>
+  <tr>
+    <td>Magazzini Alimentari Riuniti</td>
+    <td>Giovanni Rovelli</td>
+    <td>Italy</td>
+  </tr>
+  <tr>
+    <td>North/South</td>
+    <td>Simon Crowther</td>
+    <td>UK</td>
+  </tr>
+  <tr>
+    <td>Paris spécialités</td>
+    <td>Marie Bertrand</td>
+    <td>France</td>
+  </tr>
+</table>
+    `
+    return area;
+  },
+  args: {
+    label: 'Label',
+  },
+};
+
+export const Sizes: Story = {
+  render: (args) => {
+    let area = document.createElement('div');
+
+    area.innerHTML = `
+ TODO
+  `
+    return area;
+  },
+  args: {
+    label: 'Label',
+  },
+};
